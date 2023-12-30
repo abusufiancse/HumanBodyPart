8c69a0daf662cac637db7b86ff58e4bd4abeb99a
abusufiancse <abu15-2532@diu.edu.bd>
2023-12-30 22:32:23 +0600

    update

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,14 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -17,14 +22,47 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.green,
         useMaterial3: true,
       ),
-      home: SafeArea(
-        child: Center(child: HumanAnatomy(onChanged: _handleBodyPartSelection)),
-      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: HumanAnatomy(onChanged: _handleBodyPartSelection),
     );
   }
 
-  void _handleBodyPartSelection(List selectedBodyParts) {
-    // Handle the selected body parts here, if needed.
-    print('Selected Body Parts: $selectedBodyParts');
+  void _handleBodyPartSelection(String? selectedBodyPart) {
+    // Handle the selected body part here, if needed.
+    print('Selected Body Part: $selectedBodyPart');
+
+    // Show a dialog with the selected body part
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Selected Body Part'),
+          content: Text('You selected: $selectedBodyPart'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // Close the dialog
+              },
+              child: Text('OK'),
+            ),
+          ],
+        );
+      },
+    );
   }
 }
